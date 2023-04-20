33732d6960b6d81de110636401ecbd0d0062945e
Rocki_Kim <rocki.k1208@gmail.com>
2023-04-20 12:47:26 +0900

    Add : 04.14. TIS
    Hash

--- programmers/Level1/달리기 경주/solution.py
+++ programmers/Level1/달리기 경주/solution.py
@@ -1,6 +1,18 @@
-def solution(s):
-    a = len(s)
-    if a %2 != 0:
-        return s[(a//2)]
-    else:
-        return s[(a//2)-1]+s[(a//2)]
+# 시간복잡도 실패
+def solution(players, callings):
+    for calling in callings:
+        p_idx = players.index(calling)
+        players[p_idx-1:p_idx+1] = [players[p_idx], players[p_idx-1]]
+    return players
+
+# Hash로 해결
+def solution(players, callings):
+    players_dict = {v : k for k,v in enumerate(players)}
+    for calling in callings:
+        idx = players_dict[calling]
+        players_dict[calling],players_dict[players[idx-1]] = idx-1 , idx
+        players[idx], players[idx-1] = players[idx-1], players[idx]
+        print(players)
+        
+        print(players_dict)
+    return players
