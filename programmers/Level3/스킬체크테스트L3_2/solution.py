78d8bbf717b93b3b3d497592a9657294a1dbb8ad
Rocki_Kim <rocki.k1208@gmail.com>
2023-05-15 20:54:56 +0900

    Add : 05.15. TIS
    스킬테스트1

---
+++ programmers/Level3/스킬체크테스트L3_2/solution.py
@@ -0,0 +1,38 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x == root_y:
+            return False
+
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+        elif self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+        else:
+            self.parent[root_y] = root_x
+            self.rank[root_x] += 1
+
+        return True
+
+def solution(n, costs):
+    uf = UnionFind(n)
+    costs.sort(key=lambda x: x[2])  # Sort edges by cost
+    min_cost = 0
+
+    for i in range(len(costs)):
+        island1, island2, cost = costs[i]
+        if uf.union(island1, island2):
+            min_cost += cost
+
+    return min_cost
